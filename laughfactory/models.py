88cf332f98e00835406d0f1fbe21665c01d34c38
Anibal Hugo <ani2bal@yahoo.com>
2023-01-11 19:01:20 -0500

    added models

--- laughfactory/models.py
+++ laughfactory/models.py
@@ -10,3 +10,14 @@ class Comment(db.Model):
     id= db.Column(db.Integer, primary_key = True)
     submitter = db.Column(db.String(250))
     comment = db.Column(db.Text)
+
+class Meme(db.Model):
+    __tablename__ = 'memes'
+    id= db.Column(db.Integer, primary_key= True)
+    submitter = db.Column(db.String(250))
+    meme = db.Column(db.Text)
+
+class User(db.Model):
+    __tablename__= 'users'
+    id= db.Column(db.Integer, primary_key= True)
+    
