e2a6c934c1476a33d9644bf77d339a6f85393ee0
chosenbyme <chosenbyme@hotmail.com>
2023-01-15 12:50:02 -0500

    login tested

--- laughfactory/loggedin.py
+++ laughfactory/loggedin.py
@@ -1,7 +1,15 @@
 from flask import ( Blueprint, render_template, request, redirect ) 
+from . import models
+bp = Blueprint('loggedin', __name__, url_prefix="/loggedin")
 
-bp = Blueprint('loggedin', __name__, url_prefix="/name")
-
-@bp.route('/')
-def loginned():
-    return render_template('loggedin.html')
+@bp.route('/', methods=['GET', 'POST'])
+def login():
+        if request.method == "POST":
+            user_email = request.form['email'] 
+            user_password = request.form['password'] 
+            find_email = models.User.query.filter_by(email=user_email).first()
+            find_psw = models.User.query.filter_by(password=user_password).first()
+            if find_email is None or find_psw is None:
+                return render_template('login.html')
+            else:
+                return render_template('loggedin.html')
